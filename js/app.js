e34d98a953b5f099b2f2b0a8568c8c8732a76189
helenavuv <helena.jerbic@vuv.hr>
2022-03-31 11:10:53 +0200

    Pretraga

--- js/app.js
+++ js/app.js
@@ -53,3 +53,22 @@ function PromijeniBoju(){
 
 	}
 }
+
+function Pretrazi() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("pretraga");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tablica");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
